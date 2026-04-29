865a187f544122919eb6de5465749dea21e5eb6d
Ivan Caballero <ivanf_59@hotmail.com>
2026-04-29 00:01:28 -0500

    Se agrega avatar
    Se agrega un avatar en la parte de la foto de perfil usando sprites sheet

--- src/app/features/who-am-i/who-am-i.ts
+++ src/app/features/who-am-i/who-am-i.ts
@@ -1,10 +1,10 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, ElementRef, inject, signal } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { ProfileService } from './services/profile';
 
 @Component({
   selector: 'app-who-am-i',
-  imports: [NgOptimizedImage],
+  imports: [],
   templateUrl: './who-am-i.html',
   styleUrls: ['./who-am-i.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -17,6 +17,65 @@ import { ProfileService } from './services/profile';
 export class WhoAmI {
   profileService = inject(ProfileService);
   
-  // Acceso directo a la signal para el template
   userProfile = this.profileService.profile;
+
+  private readonly el = inject(ElementRef); // Inyectamos la referencia al componente
+  
+  private readonly frameIndex = signal(0);
+  private readonly totalFrames = 302; //
+  private animationInterval: any;
+  private isGoingForward = true;
+  private observer: IntersectionObserver | null = null;
+
+  protected readonly currentFrameClass = computed(() => 
+    `frame_${this.frameIndex().toString().padStart(4, '0')}`
+  );
+
+  ngOnInit() {
+    this.setupIntersectionObserver();
+  }
+
+  private setupIntersectionObserver() {
+    this.observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.startAnimation(); // Usuario entró a la sección
+        } else {
+          this.stopAnimation();  // Usuario salió de la sección
+        }
+      });
+    }, { threshold: 0.1 }); // Se activa cuando al menos el 10% es visible
+
+    this.observer.observe(this.el.nativeElement);
+  }
+
+  private startAnimation() {
+    if (this.animationInterval) return;
+    
+    this.animationInterval = setInterval(() => {
+      this.frameIndex.update(current => {
+        if (this.isGoingForward) {
+          if (current < this.totalFrames - 1) return current + 1;
+          this.isGoingForward = false;
+          return current - 1;
+        } else {
+          if (current > 0) return current - 1;
+          this.isGoingForward = true;
+          return current + 1;
+        }
+      });
+    }, 45);
+  }
+
+  private stopAnimation() {
+    if (this.animationInterval) {
+      clearInterval(this.animationInterval);
+      this.animationInterval = null;
+    }
+  }
+
+  ngOnDestroy() {
+    this.stopAnimation();
+    this.observer?.disconnect();
+  }
 }
